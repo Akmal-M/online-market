1382de8de9a6d478efb2b6cb09e2c1efd3e78bd8
Akmal-M <akmal3017445@gmail.com>
2021-06-10 19:31:37 +0500

    translate

--- Components/Navbar.js
+++ Components/Navbar.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import useTranslation from "next-translate/useTranslation";
 
 const Navbar = () => {
+    const {t} = useTranslation();
     return (
         <div>
             <div>
