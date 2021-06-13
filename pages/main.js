75b423409708358aca144918f08781e0bd36ed2a
AsliddinAbdullayev <asliddinfrontweb2006@gmail.com>
2021-06-13 12:29:15 +0500

    ProductAdd

--- pages/main.js
+++ pages/main.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import {BsPersonPlus} from "react-icons/bs";
-import BodyCard from "../Components/BodyCard/BodyCard";
 
 const Main = () => {
     return (
         <div>
-            <BodyCard/>
+
             <BsPersonPlus/>
         </div>
     );
