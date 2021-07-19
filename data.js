2e783f5ba05ddd9e6bb0b16e8471fd2ce72ae859
Akmal-M <akmal3017445@gmail.com>
2021-07-19 23:55:18 +0500

    changes

--- data.js
+++ data.js
@@ -22,10 +22,6 @@ const data = [
                 "id": "5",
                 "url": "https://imgaz1.chiccdn.com/thumb/wap/oaupload/newchic/images/A5/70/b435cecf-2d11-4f01-be6b-8d7f28e9af31.jpg.webp?s=240x320"
             },
-            {
-                "id": "6",
-                "url": "https://imgaz1.chiccdn.com/thumb/wap/oaupload/newchic/images/A5/70/b435cecf-2d11-4f01-be6b-8d7f28e9af31.jpg.webp?s=240x320"
-            },
 
 
 
